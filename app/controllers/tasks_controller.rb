06b11034895db32273288e043d1a3a4990b90ce4
EC2 Default User <ec2-user@ip-172-31-29-84.us-east-2.compute.internal>
2018-05-23 12:52:28 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+    @user = current_user
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
 
   def show
@@ -12,8 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-        @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -40,7 +44,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-        @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
